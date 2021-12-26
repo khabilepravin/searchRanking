a5ea3ca976213ab9ddeb2d4973817e5ed185fa52
Pravin <khabilepravin@gmail.com>
2021-12-26 23:01:27 +1100

    Some resilience when making network calls

--- APIProxy/SearchRankingAPIProxy.cs
+++ APIProxy/SearchRankingAPIProxy.cs
@@ -1,6 +1,8 @@
 ﻿using APIProxy.ResposeModels;
 using Flurl;
 using Flurl.Http;
+using Polly;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Threading.Tasks;
@@ -13,25 +15,50 @@ namespace APIProxy
 
         public SearchRankingAPIProxy()
         {
-           _apiBaseUrl = ConfigurationManager.AppSettings["APIBaseUrl"];
+            _apiBaseUrl = ConfigurationManager.AppSettings["APIBaseUrl"];
         }
 
         public async Task<IEnumerable<SearchRankingResult>> GetAllRankingResults(string searchTerm)
         {
-            var result = await $"{_apiBaseUrl}/searchranking/all"
-                .SetQueryParam("searchTerm", searchTerm)
-                .GetJsonAsync<IEnumerable<SearchRankingResult>>();
-
-            return result;
+            return await Policy
+                .Handle<FlurlHttpException>(IsWorthRetrying)
+                .WaitAndRetryAsync(ExponentialBackoff())
+                .ExecuteAsync(() => $"{_apiBaseUrl}/searchranking/all"
+                    .SetQueryParam("searchTerm", searchTerm)
+                    .GetJsonAsync<IEnumerable<SearchRankingResult>>());
         }
 
         public async Task<SearchRankingResult> GetRankingResultByHost(string host, string searchTerm)
-        {            
-            var result = await $"{_apiBaseUrl}/searchranking"
-                           .SetQueryParams(new { host = host, searchTerm = searchTerm })
-                           .GetJsonAsync<SearchRankingResult>();
-            
-            return result;
+        {
+            return await Policy
+                .Handle<FlurlHttpException>(IsWorthRetrying)
+                .WaitAndRetryAsync(ExponentialBackoff())
+                .ExecuteAsync(() => $"{_apiBaseUrl}/searchranking"
+                    .SetQueryParams(new { host = host, searchTerm = searchTerm })
+                    .GetJsonAsync<SearchRankingResult>());
+        }
+
+        private bool IsWorthRetrying(FlurlHttpException ex)
+        {
+            switch ((int)ex.Call.Response.StatusCode)
+            {
+                case 408:
+                case 500:
+                case 502:
+                case 504:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private IEnumerable<TimeSpan> ExponentialBackoff()
+        {
+            return new[]{
+                        TimeSpan.FromSeconds(1),
+                        TimeSpan.FromSeconds(3),
+                        TimeSpan.FromSeconds(5)
+                        };
         }
     }
 }
