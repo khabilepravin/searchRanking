f9513d8f6f04d5cfb9db26738b2c6f9e242a3404
Pravin <khabilepravin@gmail.com>
2021-12-28 22:37:53 +1100

    Placeholder authentication code

--- SearchRankingAPI/Controllers/BaseController.cs
+++ SearchRankingAPI/Controllers/BaseController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SearchRankingAPI.Controllers
 {
     [ApiController]
     [Route("api")]
+    //[Authorize]
+
     public class BaseController : ControllerBase
     {
     }
